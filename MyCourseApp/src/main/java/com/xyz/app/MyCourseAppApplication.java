2564e6a674d86cf6f22fa2bce9ea013c88204e61
YaminiAmbati97 <yaminiambati09.com>
2020-09-05 17:55:08 +0530

    FIRST COMMIT

---
+++ MyCourseApp/src/main/java/com/xyz/app/MyCourseAppApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCourseAppApplication.class, args);
+	}
+
+}
